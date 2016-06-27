fe91be1d5f4eddeb7c9844ba283f133494e14197
=Corey Hulen <corey@hulen.com>
2016-06-27 13:09:47 -0800

    PLT-2863 adding remove from team to javascript driver

--- tests/client_team.test.jsx
+++ tests/client_team.test.jsx
@@ -192,6 +192,7 @@ describe('Client.Team', function() {
                 TestHelper.fakeUser(),
                 function(user2) {
                     TestHelper.basicClient().addUserToTeam(
+                        "",
                         user2.id,
                         function(data) {
                             assert.equal(data.user_id, user2.id);
@@ -201,6 +202,22 @@ describe('Client.Team', function() {
                             done(new Error(err.message));
                         }
                     );
+                },
+                function(err) {
+                    done(new Error(err.message));
+                }
+            );
+        });
+    });
+
+    it('removeUserFromTeam', function(done) {
+        TestHelper.initBasic(() => {
+            TestHelper.basicClient().removeUserFromTeam(
+                "",
+                TestHelper.basicUser().id,
+                function(data) {
+                    assert.equal(data.user_id, TestHelper.basicUser().id);
+                    done();
                 },
                 function(err) {
                     done(new Error(err.message));
